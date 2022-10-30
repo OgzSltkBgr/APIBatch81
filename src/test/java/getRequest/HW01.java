390561b044a84d455fa22a0fc1cf8da83e442409
OgzSltkBgr <osbtoklucu@gmail.com>
2022-10-30 21:30:36 +0300

    30/10

--- src/test/java/getRequest/HW01.java
+++ src/test/java/getRequest/HW01.java
@@ -1,6 +1,17 @@
 package getRequest;
 
-public class HW01 {
+import baseUrl.AutoBaseUrl;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.Assert.*;
+
+public class HW01 extends AutoBaseUrl {
     /*
 Given
     https://automationexercise.com/api/productsList
@@ -15,4 +26,26 @@ And
 And
      There must be 12 Women, 9 Men, 13 Kids usertype in products
   */
+
+    @Test
+    public void get01() {
+        spec.pathParams("first","productsList");
+
+        Response response = given().spec(spec).when().get("/{first}");
+       // response.prettyPrint();
+        JsonPath json =response.jsonPath();
+
+        assertEquals(200,response.statusCode());
+        assertEquals("text/html; charset=utf-8",response.contentType());
+        assertEquals("HTTP/1.1 200 OK",response.getStatusLine());
+
+        List<String>women = json.getList("products.category.usertype.findAll{it.usertype=='Women'}.usertype");
+        List<String>men = json.getList("products.category.usertype.findAll{it.usertype=='Men'}.usertype");
+        List<String>kids = json.getList("products.category.usertype.findAll{it.usertype=='Kids'}.usertype");
+
+        assertEquals(12,women.size());
+        assertEquals(9,men.size());
+        assertEquals(13,kids.size());
+
+    }
 }
