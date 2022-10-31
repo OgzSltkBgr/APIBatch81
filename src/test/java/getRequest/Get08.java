3439ddd5e569429f88cb9d227f79d33bf7a4aa30
OgzSltkBgr <osbtoklucu@gmail.com>
2022-10-31 23:29:04 +0300

    31/10

--- src/test/java/getRequest/Get08.java
+++ src/test/java/getRequest/Get08.java
@@ -3,6 +3,8 @@ package getRequest;
 import baseUrl.JsonPlaceHolderBaseUrl;
 import io.restassured.response.Response;
 import org.junit.Test;
+import testData.JsonPlaceHolderTestData;
+
 import java.util.*;
 import static io.restassured.RestAssured.*;
 import static org.junit.Assert.*;
@@ -58,6 +60,35 @@ Object Maper:
 
         assertEquals(expectedData.get("userId"),actualData.get("userId"));
         assertEquals(expectedData.get("id"),actualData.get("id"));
+        assertEquals(expectedData.get("title"),actualData.get("title"));
+        assertEquals(expectedData.get("completed"),actualData.get("completed"));
+
+        assertEquals("1.1 vegur",response.header("Via"));
+        assertEquals("cloudflare",response.header("Server"));
+        assertEquals(200,response.statusCode());
+    }
+    // Dinamik Yontem
+    @Test
+    public void get01b() {
+        spec.pathParams("first","todos","second",2);
+
+
+        // Set the expected Data expectedData=Payload
+
+        JsonPlaceHolderTestData objJsonPlace = new JsonPlaceHolderTestData();
+
+        Map<String,Object>expectedData=objJsonPlace.expectedDataMethod(1,"quis ut nam facilis et officia qui",false);
+        System.out.println("expectedData = " + expectedData);
+
+
+        // Send the get request
+        Response response=given().spec(spec).when().get("/{first}/{second}");
+        response.prettyPrint();
+
+        Map<String ,Object>actualData=response.as(HashMap.class); // De-Serialization
+        System.out.println("actualData = " + actualData);
+
+        assertEquals(expectedData.get("userId"),actualData.get("userId"));
         assertEquals(expectedData.get("title"),actualData.get("title"));
         assertEquals(expectedData.get("completed"),actualData.get("completed"));
 
