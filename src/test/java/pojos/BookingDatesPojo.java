baba3fd14cdbc11fc15daeabcfe1ba7ac415588d
OgzSltkBgr <osbtoklucu@gmail.com>
2022-11-04 23:26:05 +0300

    0411

---
+++ src/test/java/pojos/BookingDatesPojo.java
@@ -0,0 +1,44 @@
+package pojos;
+
+public class BookingDatesPojo {
+
+    // Tum keyler icin private variable lar olusturuyoruz
+    private String checkin;
+    private String checkout;
+
+    // Tum parametrelerle ve parametresiz constructorlar olusturuyoruz
+    public BookingDatesPojo(String checkin, String checkout) {
+        this.checkin = checkin;
+        this.checkout = checkout;
+    }
+
+    public BookingDatesPojo() {
+    }
+
+    // Public Getter ve Setter methodlarını oluşturuyoruz
+
+    public String getCheckin() {
+        return checkin;
+    }
+
+    public void setCheckin(String checkin) {
+        this.checkin = checkin;
+    }
+
+    public String getCheckout() {
+        return checkout;
+    }
+
+    public void setCheckout(String checkout) {
+        this.checkout = checkout;
+    }
+
+    // 4) toString() methodunu olusturuyoruz
+    @Override
+    public String toString() {
+        return "BookingDatesPojo{" +
+                "checkin='" + checkin + '\'' +
+                ", checkout='" + checkout + '\'' +
+                '}';
+    }
+}
